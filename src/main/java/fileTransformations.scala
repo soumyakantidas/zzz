efc413e31fd37b073fb97b92140fa83ca0547c22
soumyakantidas <soumyakantidas007@gmail.com>
2016-11-11 19:11:39 +0530

    Initial commit

---
+++ src/main/java/fileTransformations.scala
@@ -0,0 +1,123 @@
+import java.io.File
+import java.util.concurrent.atomic.AtomicInteger
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
+import org.apache.spark.sql.hive.HiveContext
+
+
+/**
+  * Created by soumyaka on 11/10/2016.
+  */
+object fileTransformations {
+  def main(args: Array[String]): Unit = {
+    val conf =new SparkConf()
+      .setAppName("IOT-test")
+      .setMaster("local[*]")
+
+    val sc = new SparkContext(conf)
+    val hc = new HiveContext(sc)
+
+    Logger.getRootLogger().setLevel(Level.ERROR)
+
+    val dir = "D:\\Users\\soumyaka\\Desktop\\Daimler"
+
+
+    val listOfDates = getListOfFolder(dir)
+
+    for (date <- listOfDates){
+      val listOfFolders = getListOfFolder(date.getAbsolutePath)
+//      val dateString = date.getName
+
+      for(folder <- listOfFolders){
+        val lineNumber = folder.getName.substring(4,8) // 8 is exclusive
+
+        val listOfFiles = getListOfFiles(folder.getAbsolutePath)
+        listOfFiles.foreach(file => {
+          if(file.getName == "BALogFile.txt"){
+            val BALogRDD = sc.textFile(file.getAbsolutePath)
+            val header = BALogRDD.first()
+            val headerLength = header.split(";").size
+            val RDDLength = (BALogRDD.count() - 1).toInt
+            val currRow = new AtomicInteger(0)
+            val currCol = new AtomicInteger(0)
+
+            val masterArray = ArrayBuffer.fill(headerLength, RDDLength)("")
+
+            val rowData = BALogRDD.filter(_ != header)
+              .map(_ + "a")
+              .map(line => {
+                val someArray = line.split(";")
+                val slicedArray = someArray.slice(0, someArray.size - 1)
+                slicedArray
+              }).collect()
+              /*.map(arr => {
+                while
+
+              })*/
+
+
+
+
+
+
+/*/*            val rowData = BALogRDD.filter( _ != header)
+              .map(_ + "a")
+              .map(line => {
+                val someArray = line.split(";")
+                val slicedArray = someArray.slice(0, someArray.size - 1)
+                val rowList = new ListBuffer[String]
+                var indexCount = 0
+                while(indexCount + structureSize - 1 < slicedArray.size){
+                  rowList += lineNumber + slicedArray.slice(indexCount, structureSize + indexCount).mkString(";")*/
+
+//                  hc.sql("insert into table values(" + rowList(0),  + ")");
+
+
+                  indexCount += structureSize
+                }
+                rowList
+            }).repartition(1)
+            rowData.saveAsTextFile("D:\\Daimler\\src\\main\\resources\\testOutput\\" + date.getName + "\\" + folder.getName)*/
+
+
+
+           /* val BALogRDDData = BALogRDD.filter(_ != header)
+            val splitData = BALogRDDData.map(line => {
+              val array = line.split(";")
+
+            })*/
+
+          }
+        })
+      }
+    }
+
+
+
+
+  }
+
+  def getListOfFolder(dir: String): List[File] = {
+    val d = new File(dir)
+    if (d.exists && d.isDirectory) {
+      val folders = d.listFiles.filter(_.isDirectory).toList
+      folders
+    } else {
+      List[File]()
+    }
+  }
+
+  def getListOfFiles(dir: String): List[File] = {
+    val d = new File(dir)
+    if (d.exists && d.isDirectory) {
+      val files = d.listFiles.filter(_.isFile).toList
+      files
+    } else {
+      List[File]()
+    }
+  }
+
+}
